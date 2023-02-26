377c58ca76cb404fa93f6664679dfe6c9599191a
luanalmeida18 <luanfelipe.lfpa@gmail.com>
2023-02-26 12:14:28 -0300

    Primeiro Commit Ruby

---
+++ projeto1.rb
@@ -0,0 +1,10 @@
+print "Digite seu Nome: "
+nome = gets.chomp
+
+print "Digite seu Sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+
+puts "O leitor #{nome} #{sobrenome} tem #{idade} anos."
